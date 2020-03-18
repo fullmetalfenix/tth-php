c83112bebd81f1355ff6f84f6dd6906d710807a8
Jason <Jpihlk@yahoo.com>
2020-03-18 10:03:06 -0400

    templated, dynamic titles and active navigation

---
+++ suggest.php
@@ -0,0 +1,11 @@
+<?php 
+$pageTitle = "Suggest a Media Item";
+$section = "suggest";
+include("inc/header.php"); ?>
+
+<div>
+    <h1>Suggest A Media Item</h1>
+</div>
+
+
+<?php include("inc/footer.php"); ?>
