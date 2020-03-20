33b427d5627a915f4b0b67233b9e44cc7d71e795
Jason <Jpihlk@yahoo.com>
2020-03-19 22:08:57 -0400

    setting up / adding array to catalog

--- catalog.php
+++ catalog.php
@@ -1,5 +1,14 @@
 <?php 
+$catalog = array();
+
+// Dont try this method of building an array in JS!
+$catalog[101] = "Design Patterns";
+$catalog[201] = "Forrest Gump"; 
+$catalog[301] = "Beethoven";
+$catalog[102] = "Clean Code";
+
 $pageTitle = "Full Catalog";
+
 $section = null;
 
 if(isset($_GET["cat"])){
@@ -19,8 +28,16 @@ if(isset($_GET["cat"])){
 
 include("inc/header.php"); ?>
 
-<div class="section page">
-    <h1><?php echo $pageTitle; ?></h1>
+<div class="section catalog page">
+    <div class="wrapper">
+        <h1><?php echo $pageTitle; ?></h1>
+        <ul>
+            <?php foreach($catalog as $item){
+                echo "<li>" . $item . "</li>";
+            };
+            ?>
+        </ul>
+    </div>
 </div>
 
 
